39ab40ac28393e00e63a24ca75780c0a35dfdb60
gloomydumber <chromacrow@naver.com>
2022-08-01 23:09:05 +0900

    Co-authored-by: HyoWon Jang <jang.hw73@gmail.com>

--- test/filterTickers.js
+++ test/filterTickers.js
@@ -1,2 +1,47 @@
 // https://api.upbit.com/v1/market/all
 // https://api.binance.com/api/v3/exchangeInfo
+
+global.WebSocket = require("ws");
+global.XMLHttpRequest = require("xhr2"); // for Server Side Ajax
+const { KRW } = require("./krw");
+const { USDT } = require("./usdt");
+const { TICKERS } = require("./tickers");
+const { webSocket } = require("rxjs/webSocket");
+const { ajax } = require("rxjs/ajax");
+const {
+  map,
+  mergeMap,
+  switchMap,
+  pluck,
+  retry,
+  tap,
+  combineLatestWith,
+  retryWhen,
+} = require("rxjs/operators");
+const { v4 } = require("uuid");
+const {
+  combineLatest,
+  timer,
+  defer,
+  of,
+  filter,
+  share,
+  Subject,
+  interval,
+  Observable,
+} = require("rxjs");
+
+const upbitObs = timer(0, 2000).pipe(
+  switchMap(() => ajax("https://api.upbit.com/v1/market/all")),
+  pluck("response"),
+  retry({ delay: 1500 })
+);
+
+const binanceObs = timer(0, 2000).pipe(
+  switchMap(() => ajax("https://api.binance.com/api/v3/exchangeInfo")),
+  pluck("response"),
+  retry({ delay: 1500 })
+);
+
+// upbitObs.subscribe(console.log);
+binanceObs.subscribe(console.log);
---
+++ test/fromGithub.js
@@ -0,0 +1,104 @@
+global.WebSocket = require("ws");
+global.XMLHttpRequest = require("xhr2"); // for Server Side Ajax
+const { KRW } = require("./krw");
+const { USDT } = require("./usdt");
+const { TICKERS } = require("./tickers");
+const { webSocket } = require("rxjs/webSocket");
+const { ajax } = require("rxjs/ajax");
+const {
+  map,
+  mergeMap,
+  switchMap,
+  pluck,
+  retry,
+  tap,
+  combineLatestWith,
+  retryWhen,
+  repeat,
+  repeatWhen,
+  delayWhen,
+} = require("rxjs/operators");
+const { v4 } = require("uuid");
+const {
+  combineLatest,
+  timer,
+  defer,
+  of,
+  filter,
+  share,
+  Subject,
+  interval,
+  Observable,
+} = require("rxjs");
+
+const RECONNECT_INTERVAL = 3000;
+const webSocketSubject = webSocket("ws://localhost:8001");
+// webSocketSubject
+//   .pipe(
+//     repeatWhen((errors) =>
+//       errors.pipe(
+//         tap((val) =>
+//           console.log("[retry] Try to reconnect ", val, " ", Date.now())
+//         ),
+//         delayWhen((_) => timer(RECONNECT_INTERVAL))
+//       )
+//     )
+//   )
+//   .subscribe((value) => console.log("[retry] Receive ", value));
+
+// webSocketSubject
+//   .pipe(repeat({ delay: 1500 }))
+//   .subscribe((value) => console.log("[retry] Receive ", value));
+
+// webSocketSubject
+//   .pipe(retry({ delay: 1500 }))
+//   .subscribe((value) => console.log("[retry] Receive ", value));
+
+// webSocketSubject
+//   .pipe(repeat({ delay: 1500 }), retry({ delay: 1500 }))
+//   .subscribe((value) => console.log("[retry] Receive ", value));
+
+// webSocketSubject
+//   .pipe(repeat({ delay: 1500 }), retry({ delay: 1500 }))
+//   .subscribe({
+//     next: (value) => console.log("[retry] Receive ", value),
+//     error: (err) => console.log(err),
+//   });
+
+webSocketSubject
+  .pipe(
+    repeat({
+      delay: (x) =>
+        of(`${x}회 repeat 하였습니다`).pipe(
+          tap((x) => console.log(x)),
+          delayWhen((_) => timer(RECONNECT_INTERVAL))
+        ),
+    }),
+    retry({
+      delay: (err, x) =>
+        of(`${x}회 retry 하였습니다`).pipe(
+          tap((x) => console.log(x)),
+          delayWhen((_) => timer(RECONNECT_INTERVAL))
+        ),
+    })
+  )
+  .subscribe({
+    next: (value) => console.log("[retry] Receive ", value),
+    error: (err) => console.log(err),
+  });
+
+// webSocketSubject
+//   .pipe(
+//     repeat((errors) =>
+//       errors.pipe(
+//         tap((val) =>
+//           console.log("[retry] Try to reconnect ", val, " ", Date.now())
+//         ),
+//         delayWhen((_) => timer(RECONNECT_INTERVAL))
+//       )
+//     )
+//   )
+//   .subscribe((value) => console.log("[retry] Receive ", value));
+
+// retry : 최초 접속이 되지 않으면, 재시도, 일단 연결이 되고나서 끊기는 연결은 재시도 하지 않음
+// repeat : 최초 접속이 되지 않으면, 에러 Emit 후 종료
