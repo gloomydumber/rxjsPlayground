84ad49015ac59f974644cf1f10cf2c26dc7ca819
gloomydumber <chromacrow@naver.com>
2022-08-04 01:58:37 +0900

    commonTickers

--- test/filterTickers.js
+++ test/filterTickers.js
@@ -18,6 +18,7 @@ const {
   combineLatestWith,
   retryWhen,
   distinct,
+  reduce,
 } = require("rxjs/operators");
 const { v4 } = require("uuid");
 const {
@@ -31,26 +32,55 @@ const {
   Subject,
   interval,
   Observable,
+  merge,
+  skip,
+  take,
+  toArray,
+  forkJoin,
 } = require("rxjs");
 
 const upbitObs = timer(0, 2000).pipe(
   switchMap(() => ajax("https://api.upbit.com/v1/market/all")),
   pluck("response"),
-  mergeMap((x) => from(x).pipe(map((x) => x.market.split("-")[1]))),
-  distinct()
+  mergeMap((x) =>
+    from(x).pipe(
+      map((x) => x.market.split("-")[1]),
+      distinct(),
+      toArray()
+    )
+  )
   // retry({ delay: 1500 })
 );
 
 const binanceObs = timer(0, 2000).pipe(
   switchMap((_) => ajax("https://api.binance.com/api/v3/exchangeInfo")),
   pluck("response", "symbols"),
-  mergeMap((x) => from(x).pipe(map((x) => x.baseAsset))),
-  distinct()
+  mergeMap((x) =>
+    from(x).pipe(
+      map((x) => x.baseAsset),
+      distinct(),
+      toArray()
+    )
+  )
+
   // retry({ delay: 1500 })
 );
 
-upbitObs.subscribe(console.log);
-// binanceObs.subscribe(console.log);
+// upbitObs.subscribe();
+// binanceObs.subscribe();
+
+// const filteredArray = array1.filter(value => array2.includes(value));
+const filteredArray = combineLatest({
+  upbitTicker: upbitObs,
+  binanceTicker: binanceObs,
+}).pipe(
+  map((x) => x.upbitTicker.filter((value) => x.binanceTicker.includes(value)))
+);
+
+filteredArray.subscribe(console.log);
+
+// merge(upbitObs, binanceObs).subscribe(console.log);
+// merge(upbitObs, binanceObs).pipe(skip(1), take(1)).subscribe(console.log);
 
 // Binance 상장 coin 정보
 // status가 BREAK, TRADING
